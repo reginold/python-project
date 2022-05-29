8536a1edb44f7180bda715c2e8a3f2bb84ed9f90
Bowen Lu <lu.bowen@ridgelinez.com>
2022-05-29 07:46:11 +0000

    feat: add the intro of the static-classmethod

---
+++ python-tips/002-super-inheritor/super_advanced.py
@@ -0,0 +1,2 @@
+# Supercharge Your Classes With Python super()
+# https://realpython.com/python-super/#what-can-super-do-for-you
---
+++ python-tips/003-static-classmethod/static-classmethod.py
@@ -0,0 +1,3 @@
+# python的类的 静态属性 类方法 静态方法 
+# https://www.cnblogs.com/gouguoqilinux/p/9194093.html
+# or https://www.zhihu.com/question/20021164
