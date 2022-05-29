d66c1166dd44375bee7404f6e47b8daf9ec31f46
Bowen Lu <lu.bowen@ridgelinez.com>
2022-05-29 06:52:15 +0000

    feat: update the super pytho intermediate

--- python-tips/002-super-inheritor/super_intermediate.py
+++ python-tips/002-super-inheritor/super_intermediate.py
@@ -10,16 +10,20 @@ class Base(object):
         print("leave BaseClass")
     
     def greet_base(self, speaker, content):
-        speaker_name = self.nation + "." + self.school + "." +  speaker
+        speaker_name = self.nation + "." + self.school + "." + speaker
         speaker_content = content
-        res = speaker_name + " says " + speaker_content
+        res = "BaseCls " + speaker_name + " says " + speaker_content
         return res
 
 class A(Base):
-    def __init__(self):
+    def __init__(self, nation: str):
+        self.nation = nation
         print("enter A based BaseClass")
         super(A, self).__init__()
         print("leave A based BaseClass")
+
+    def call_init(self):
+        return "Call the base init val" + self.nation
 
 class B(Base):
     def __init__(self):
@@ -30,11 +34,41 @@ class B(Base):
 class C(A, B):
     def __init__(self):
         print("enter C based BaseClass")
-        super(C, self).__init__()
+        super(C, self).__init__(self)
         print("leave C based BaseClass")
+
+    def greet_base(self, speaker, content):
+        speaker_name = self.nation + "." + self.school + "." + speaker
+        speaker_content = content
+        res = "Cls C " + speaker_name + " says " + speaker_content
+        return res
 
         
 c = C()
+a = A("China")
 res = c.greet_base("lu", "Thank you for your coming...")
-print(res)
+print(a.call_init())
+print("BREAKING NEWS: " + res)
 print(C.mro())
+
+# result:
+# 1. revoke the greet_base on BaseCls
+# 2. add the new func in A cls
+
+
+# enter C based BaseClass
+# enter A based BaseClass
+# enter B based BaseClass
+# enter BaseClass
+# leave BaseClass
+# leave B based BaseClass
+# leave A based BaseClass
+# leave C based BaseClass
+# enter A based BaseClass
+# enter BaseClass
+# leave BaseClass
+# leave A based BaseClass
+# Call the base init valJapan
+# BREAKING NEWS: Cls C Japan.UEC university.lu says Thank you for your coming...
+# [<class '__main__.C'>, <class '__main__.A'>, <class '__main__.B'>, <class '__main__.Base'>, <class 'object'>]
+
