ab22f53ca67d39eb7453a87d24908da2d23eb226
Bowen Lu <reginold.lu@gmail.com>
2022-06-08 20:20:26 +0900

    feat: add the @property func basic

---
+++ python-tips/005-@property/property_funda.py
@@ -0,0 +1,34 @@
+# circle.py
+
+class Circle:
+    def __init__(self, radius):
+        self._radius = radius
+
+    def _get_radius(self):
+        print("Get radius")
+        return self._radius
+
+    def _set_radius(self, value):
+        print("Set radius")
+        self._radius = value
+
+    def _del_radius(self):
+        print("Delete radius")
+        del self._radius
+
+    radius = property(
+        fget=_get_radius,
+        fset=_set_radius,
+        fdel=_del_radius,
+        doc="The radius property."
+    )
+
+circle = Circle(5.0)
+print(circle.radius)
+circle.radius = 100.0
+print(circle.radius)
+
+# del circle.radius
+# print(circle.radius)
+
+help(circle)
